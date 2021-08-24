b54f27ce8105f72229519bba86796ee4c54b69c0
IFuckingHatePikeys <Shyliko315on@gmail.com>
2021-08-24 11:03:24 +0700

    Add 16 SON in SON-changer

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     private double value;
     boolean flag;
     Scanner scan;
-    int[] NumberInFinalSON = new int[100];
+    String[] NumberInFinalSON = new String[100];
 
         
         
@@ -18,31 +18,78 @@ public class Calculator {
         scan = new Scanner(System.in);
         flag = false;
         for(int i =0;i<100;i++){
-            NumberInFinalSON[i] = 0;
+            NumberInFinalSON[i] = "0";
         }
     }
     public int getRandomNumFromA_ToB(int min,int max){
         return(min + (int)(Math.random() * (max-min+1)));
     }
     
-    private void ChangeScaleOfNotation(int Number,int StartSON, int FinalSON){
-        int LastCipher;
+    private void ChangeScaleOfNotation(String StrNumber,int StartSON, int FinalSON){
         int NumberIn10SON = 0;
-        int lenght = Integer.toString(Number).length();
+        int[] Number = new int[StrNumber.length()];
+        for(int i = 0;i<StrNumber.length(); i++){            
+            switch (String.valueOf(StrNumber.charAt(StrNumber.length() - i -1))) {
+                case "A":
+                    Number[i]=10;
+                    break;
+                case "B":
+                    Number[i]=11;
+                    break;
+                case "C":
+                    Number[i]=12;
+                    break;
+                case "D":
+                    Number[i]=13;
+                    break;
+                case "E":
+                    Number[i]=14;
+                    break;
+                case "F":
+                    Number[i]=15;
+                    break;
+                default:
+                    Number[i] = Integer.parseInt(String.valueOf(StrNumber.charAt(StrNumber.length() - i -1)));
+                    break;
+            }
+        }
+        for(int i = 0;i<StrNumber.length();i++){
+            System.out.println(Number[i]);
+        }
         //convert to 10-Scale of location
-        for(int i = 0;i<lenght;i++){
-            LastCipher = Number%10;
-            NumberIn10SON += LastCipher*(Math.pow(StartSON, i));
-            Number = Number/10;
+        for(int i = 0;i<StrNumber.length();i++){
+            NumberIn10SON += Number[i]*(Math.pow(StartSON, i));
         }
         int i=0;
         while(NumberIn10SON!=0){
-            NumberInFinalSON[i]= NumberIn10SON%FinalSON;
+            switch (NumberIn10SON%FinalSON) {
+                case 10:
+                    NumberInFinalSON[i] = "A";
+                    break;
+                case 11:
+                    NumberInFinalSON[i] = "B";
+                    break;
+                case 12:
+                    NumberInFinalSON[i] = "C";
+                    break;
+                case 13:
+                    NumberInFinalSON[i] = "D";
+                    break;
+                case 14:
+                    NumberInFinalSON[i] = "E";
+                    break;
+                case 15:
+                    NumberInFinalSON[i] = "F";
+                    break;
+                default:
+                    NumberInFinalSON[i] = String.valueOf(NumberIn10SON%FinalSON);
+                    break;
+            }
             NumberIn10SON = NumberIn10SON/FinalSON;
             i++;
         }
         for(int j =99;j>=0;j--){
-            if(NumberInFinalSON[j]!=0)
+            if(NumberInFinalSON[j].equals("0") == false)
                 flag = true;
             if(flag == true){
                 System.out.print(NumberInFinalSON[j]);
@@ -72,7 +119,7 @@ public class Calculator {
         System.out.println("а потом систему счисления, в которую число нужно перевести.");
         System.out.println("");
         System.out.println("Например 10 2 10, означает: переведи число 10 в двоичной системе в десятичную систему.");
-        ChangeScaleOfNotation(scan.nextInt(), scan.nextInt(), scan.nextInt());
+        ChangeScaleOfNotation(scan.next(), scan.nextInt(), scan.nextInt());
     }
  
     
