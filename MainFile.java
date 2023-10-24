45d7cfca986b6afe01f4ba19fd228fa4edc9f7a1
youngmyn <Shyliko315on@gmail.com>
2023-10-25 00:28:25 +0300

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -14,14 +14,12 @@ public class MainFile {
     public static void main(String[] args) {        
         int count = 0;
         double result;
-          for(int i = 0;i<net;i++){
-              Patience patience = new Patience();
-              if(patience.IsPatienceSuccessful() == true){
-                  count++;
-              }
-          }
-          result = 1.0*count/net;
-          System.out.println(result*100 + " Процента");
+        for(int i = 0; i<net; i++){
+            Patience patience = new Patience();
+            if(patience.IsPatienceSuccessful()) count++;
+        }
+        result = 1.0*count/net;
+        System.out.println(result*100 + " Процента");
     }
     
 }
